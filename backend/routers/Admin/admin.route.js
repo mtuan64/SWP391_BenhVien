c1553ecdb64f9c6350bd2b8b725def1658bc322d
mtuan64 <tuanodaynay@gmail.com>
2025-06-19 19:53:25 +0700

    fix conflict merge

--- backend/routers/Admin/admin.route.js
+++ backend/routers/Admin/admin.route.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const adminRouter = express.Router();
 
+const {
+  getUserAccs,
+  editUsers,
+  delUsers,
+  getEmployees,
+  editEmployees,
+  delEmployees,
+  createEmployees,
+  changeStatus,
+} = require("../../controller/admin/adminService");
 
+// Admin - user manage
+apmRouter.get("/users", getUserAccs);
+apmRouter.put("/updUser/:id", editUsers);
+apmRouter.put("/changeStatus/:id", changeStatus); // Assuming this is for changing user status
+apmRouter.delete("/delUser/:id", delUsers);
+
+// Admin - employee manage
+apmRouter.get("/employees", getEmployees);
+apmRouter.put("/updEmp/:id", editEmployees);
+apmRouter.delete("/delEmp/:id", delEmployees);
+apmRouter.post("/createEmp", createEmployees);
 
 module.exports = adminRouter;
