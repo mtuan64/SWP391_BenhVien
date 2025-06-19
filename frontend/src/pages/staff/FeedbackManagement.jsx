f9626deb8cfacd67e29df94601c85c19467e543b
HoangHHE176425 <hoangxmhg2003@gmail.com>
2025-06-19 16:24:23 +0700

    Add staff pages

---
+++ frontend/src/pages/staff/FeedbackManagement.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FeedbackManagement = () => {
+  return (
+    <div>
+      <h1>Feedback Management </h1>
+      <p>Welcome to the Feedback Management.</p>
+    </div>
+  );
+};
+
+export default FeedbackManagement;
