a310de9ec5f7ccb2f3de9e61cf8648e1788b9c04
primaldev <binhbestleeseen@gmail.com>
2025-06-20 15:34:35 +0700

    merge staff v2

--- backend/routers/auth/auth.route.js
+++ backend/routers/auth/auth.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authRouter = express.Router();
 const { loginUser, registerUser } = require('../../controller/auth/authServices');
 
-authRouter.post('/login', loginUser);
-authRouter.post('/register', registerUser);
+// authRouter.post('/login', loginUser);
+// authRouter.post('/register', registerUser);
 
 module.exports = authRouter;
