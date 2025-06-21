c480f897bc1e69382820417aad2511844ea83f95
HoangHHE176425 <hoangxmhg2003@gmail.com>
2025-06-21 08:46:16 +0700

    Remove checkup.route.js - logic migrated into Doctor and Patient workflows

--- backend/routers/checkup/checkup.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createCheckup } = require('../../controller/checkup/checkUpServices');
-const checkupRouter = express.Router();
-
-checkupRouter.post('/create', createCheckup);
-
-module.exports = checkupRouter;
