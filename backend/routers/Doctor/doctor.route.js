40682b1fdf171d66ba7435f98c3056f0c4636655
HuyPb29 <huypb2911@gmail.com>
2025-07-01 00:47:04 +0700

    API get doctor

--- backend/routers/Doctor/doctor.route.js
+++ backend/routers/Doctor/doctor.route.js
@@ -1,11 +1,12 @@
 // backend/routers/Doctor/doctor.route.js
 const express = require("express");
 const router = express.Router();
+const {getAllDoctors} = require("../../controller/doctor/doctorService");
 
 // Route mẫu
 router.get("/", (req, res) => {
   res.send("Doctor route is working!");
 }); 
+router.get("/doctor", getAllDoctors);
 
 module.exports = router;
-
