4b4f77eb46af64fdd899d2dc9607374e6cae5d3f
HuyPb29 <huypb2911@gmail.com>
2025-07-01 00:47:47 +0700

    Update user.route.js

--- backend/routers/User/user.route.js
+++ backend/routers/User/user.route.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const userRouter = express.Router();
-
 const {
   createAppointment,
-  getAppointments,
-  cancelAppointment,
-  rescheduleAppointment,
-  getAppointmentHistory,
-  sendReminders
+  getAppointmentsByUser,
+  cancelAppointment
 } = require("../../controller/user/userService");
+const { authMiddleware } = require("../../middleware/auth.middleware");
 
-userRouter.post("/create", createAppointment);
-userRouter.get("/appointments", getAppointments);
-userRouter.put("/appointments/:id/cancel", cancelAppointment);
-userRouter.put("/appointments/:id/reschedule", rescheduleAppointment);
-userRouter.get("/appointments/history", getAppointmentHistory);
-userRouter.post("/appointments/reminders/send", sendReminders);
 userRouter.get('/', (req, res) => {
   res.send("User route is working!");
 });
 
+userRouter.post('/create', authMiddleware, createAppointment);
+userRouter.get('/user', authMiddleware, getAppointmentsByUser);
+userRouter.post('/cancel/:id', authMiddleware, cancelAppointment);
 
 module.exports = userRouter;
