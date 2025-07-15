34549bfde0dc18c24d4aebad32ffe0ba4a451414
ohrax <iamyungrax@gmail.com>
2025-07-15 21:01:53 +0700

    thêm button reference cho detail thuốc

--- frontend/src/pages/MedicineDetail.jsx
+++ frontend/src/pages/MedicineDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import "../assets/css/MedicineDetail.css";
@@ -12,6 +12,26 @@ const MedicineDetail = () => {
     const [medicine, setMedicine] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const typeRef = useRef(null);
+    const groupRef = useRef(null);
+    const ingredientRef = useRef(null);
+    const indicationRef = useRef(null);
+    const contraindicationRef = useRef(null);
+    const dosageRef = useRef(null);
+    const sideEffectsRef = useRef(null);
+    const precautionRef = useRef(null);
+    const interactionRef = useRef(null);
+    const storageRef = useRef(null);
+    const scrollToTarget = (targetRef) => {
+        const headerOffset = 160; // chiều cao header cố định
+        const elementPosition = targetRef.current.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    }
 
     useEffect(() => {
         const fetchMedicine = async () => {
@@ -68,43 +88,65 @@ const MedicineDetail = () => {
             <div className="medicine-detail-container">
                 <h1 className="medicine-detail-title">{medicine.name}</h1>
 
-                <div className="medicine-detail-meta">
-                    <span><b>Dạng bào chế:</b> {medicine.type || "-"}</span>
-                    <span><b>Nhóm thuốc:</b> {medicine.group || "-"}</span>
-                    <span><b>Hoạt chất:</b> {medicine.ingredient || "-"}</span>
-                </div>
+            <div className="medicine-detail-ref">
+                <button onClick={() => scrollToTarget(typeRef)}>Dạng bào chế</button>
+                <button onClick={() => scrollToTarget(groupRef)}>Nhóm thuốc</button>
+                <button onClick={() => scrollToTarget(ingredientRef)}>Hoạt chất</button>
+                <button onClick={() => scrollToTarget(indicationRef)}>Chỉ định</button>
+                <button onClick={() => scrollToTarget(contraindicationRef)}>Chống chỉ định</button>
+                <button onClick={() => scrollToTarget(dosageRef)}>Liều dùng</button>
+                <button onClick={() => scrollToTarget(sideEffectsRef)}>Tác dụng phụ</button>
+                <button onClick={() => scrollToTarget(precautionRef)}>Thận trọng</button>
+                <button onClick={() => scrollToTarget(interactionRef)}>Tương tác thuốc</button>
+                <button onClick={() => scrollToTarget(storageRef)}>Bảo quản</button>
+            </div>
 
-                <section className="medicine-detail-section">
+                <section ref={ typeRef } className="medicine-detail-section">
+                    <h2>Dạng bào chế</h2>
+                    <div>{medicine.type || "-"}</div>
+                </section>
+
+                <section ref={ groupRef } className="medicine-detail-section">
+                    <h2>Nhóm thuốc</h2>
+                    <div>{medicine.group || "-"}</div>
+                </section>
+
+                <section ref={ ingredientRef } className="medicine-detail-section">
+                    <h2>Hoạt chất</h2>
+                    <div>{medicine.ingredient || "-"}</div>
+                </section>
+
+                <section ref={ indicationRef } className="medicine-detail-section">
                     <h2>Chỉ định</h2>
                     <div>{medicine.indication || "-"}</div>
                 </section>
 
-                <section className="medicine-detail-section">
+                <section ref={ contraindicationRef } className="medicine-detail-section">
                     <h2>Chống chỉ định</h2>
                     <div>{medicine.contraindication || "-"}</div>
                 </section>
 
-                <section className="medicine-detail-section">
+                <section ref={ dosageRef } className="medicine-detail-section">
                     <h2>Liều dùng</h2>
                     <div>{medicine.dosage || "-"}</div>
                 </section>
 
-                <section className="medicine-detail-section">
+                <section ref={ sideEffectsRef } className="medicine-detail-section">
                     <h2>Tác dụng phụ</h2>
                     <div>{medicine.sideEffects || "-"}</div>
                 </section>
 
-                <section className="medicine-detail-section">
+                <section ref={ precautionRef } className="medicine-detail-section">
                     <h2>Thận trọng</h2>
                     <div>{medicine.precaution || "-"}</div>
                 </section>
 
-                <section className="medicine-detail-section">
+                <section ref={ interactionRef } className="medicine-detail-section">
                     <h2>Tương tác thuốc</h2>
                     <div>{medicine.interaction || "-"}</div>
                 </section>
 
-                <section className="medicine-detail-section">
+                <section ref={ storageRef } className="medicine-detail-section">
                     <h2>Bảo quản</h2>
                     <div>{medicine.storage || "-"}</div>
                 </section>
