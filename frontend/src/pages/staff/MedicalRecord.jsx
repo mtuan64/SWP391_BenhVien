1b9c92da9fc890c3cf20ce24e84fa3808063e9ed
sonmhhe172086 <sonmhhe172086@fpt.edu.vn>
2025-07-24 00:52:36 +0700

    update footer

--- frontend/src/pages/staff/MedicalRecord.jsx
+++ frontend/src/pages/staff/MedicalRecord.jsx
@@ -39,7 +39,7 @@ function MedicalRecord() {
     if (!value) {
       return Promise.reject(new Error("Vui lòng nhập số CMND/CCCD!"));
     }
-    const identityNumberRegex = /^[0-9]{6}$/;
+    const identityNumberRegex = /^[0-9]{12}$/;
     if (!identityNumberRegex.test(value)) {
       return Promise.reject(new Error("Số CMND/CCCD phải là 6 ký tự số, không chứa chữ, không khoảng trắng và không ký tự đặc biệt!"));
     }
